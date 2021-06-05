4bded03967c160233cccfeecb9620cba5d5aa254
Justin <aquino.j94@gmail.com>
2021-06-04 21:40:49 -0700

    Added CORS middleware to server

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"jaquino94/golang-react/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/hello", routes.Hello)
 	app.Get("/", routes.Home)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from './components/header'
 import JobList from './components/Joblist/joblist-component';
 
 function App() {
-  let [message, setMessage] = useState('');
+    let [state, setState] = useState({});
 
-    fetch('http://localhost:8000/hello')
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err)) 
+    useEffect(() => {
+        fetch('http://localhost:8000/hello')
+          .then(res => res.json())
+          .then(data => setState(data))
+          .catch(err => console.log(err))
+    },[]);
 
-  return (
-    <div className="App">
-      <Header header="Applied Jobs" />
-      <div>
-        <JobList/>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+          <Header header={state.Message} />
+          <div>
+            <JobList/>
+          </div>
+        </div>
+    );
 }
 
 export default App;
